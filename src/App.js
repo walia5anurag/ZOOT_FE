40377d92817adef4162930b0238a1b155b3c36d7
anurag55555 <anurag.walia@nineleaps.com>
2021-01-18 21:34:51 +0530

    [Authentication] | feature | Boiler plate code for create account

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import CreateAccount from './authentication/createAccount/index'
+function App() {
+  return (
+    <div className="App">
+     <CreateAccount/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/authentication/createAccount/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import '../createAccount/index.css'
+
+class createAccount extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+        <div>
+           <div class='container'>
+             <div class='headingContainer'>
+                <div class='headingText'>
+                        <span>ZOOT</span><br/>
+                        
+                </div>
+                <div class='headingSubText'>
+                    <span>CREATE YOUR ACCOUNT</span>
+                </div>
+              </div>
+              
+            </div>
+
+            <div class='form'>
+                
+            </div>
+        </div>
+        )
+    }
+}
+export default createAccount
